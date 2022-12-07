1bc0df5b1897ea22b8de8a05059084def2c8ace0
diego-bittencourt <mendesdiego90@gmail.com>
2022-12-07 22:21:12 +0900

    add dummy text to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,15 @@
 <template>
     <h1>ABOUT</h1>
     <p>This is the about page</p>
+    <p>This page will load blog posts, I guess.
+        I need you to decide
+    </p>
 </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
